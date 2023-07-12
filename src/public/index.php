9a3a262290e958268b69a381ec0e64b6d0f8eaee
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-12 23:20:00 +0900

    課題①の提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,16 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$food = 7;
+$num = 0;
+if($food % 2 == 0){
+    $num = $food / 2;
+}else{
+    $num = ($food - 1) / 2;
+    if($num > 3){
+        $num += 5;
+    }else{
+        $num += 5;
+    }
+}
+echo $num;
+
+var_dump($num == 8);
