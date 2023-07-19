ae669b37c05118ed16f2ca1ffaa769dee811b3bb
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-19 23:25:05 +0900

    classを用いて修正

--- src/public/index.php
+++ src/public/index.php
@@ -1,16 +1,43 @@
 <?php
-$food = 7;
-$num = 0;
-if($food % 2 == 0){
-    $num = $food / 2;
-}else{
-    $num = ($food - 1) / 2;
-    if($num > 3){
-        $num += 5;
-    }else{
-        $num += 5;
+class FoodCalculator {
+    private $food;
+
+    public function __construct($food) {
+        $this->food = $food;
+    }
+
+    public function calculateNum() {
+        if ($this->food % 2 === 0) {
+            $num = $this->food / 2;
+        } else {
+            $num = ($this->food - 1) / 2;
+            if ($num > 3) {
+                $num += 6;
+            } else {
+                $num += 5;
+            }
+        }
+        return $num;
     }
 }
+
+$food = 7;
+$foodCalculator = new FoodCalculator($food);
+$num = $foodCalculator->calculateNum();
 echo $num;
 
-var_dump($num == 8);
+
+
+// $food = 7;
+// $num = 0;
+// if($food % 2 == 0){
+//     $num = $food / 2;
+// }else{
+//     $num = ($food - 1) / 2;
+//     if($num > 3){
+//         $num += 5;
+//     }else{
+//         $num += 5;
+//     }
+// }
+// echo $num;
