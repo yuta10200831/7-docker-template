8f13fcec4aa9c85d8441dc98d4413784467008a9
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-19 23:06:29 +0900

    classを用いる形に修正

--- src/public/Q2.php
+++ src/public/Q2.php
@@ -1,11 +1,38 @@
 <?php
-$num = 72;
+class NumberChecker {
+  private $num;
 
-if ($num % 12 == 0 && $num % 24 == 0) {
-  echo "numの値は12の倍数かつ、24の倍数です";
-} elseif ($num % 12 == 0) {
-  echo "numの値は12の倍数です";
-} elseif ($num % 24 == 0) {
-  echo "numの値は24の倍数です";
+  public function __construct($num) {
+    $this->num = $num;
+  }
+
+  public function checkMultiples() {
+    if ($this->num % 12 === 0 && $this->num % 24 === 0) {
+        return "numの値は12の倍数かつ、24の倍数です";
+    } elseif ($this->num % 12 === 0) {
+        return "numの値は12の倍数です";
+    } elseif ($this->num % 24 === 0) {
+        return "numの値は24の倍数です";
+    }
+  }
 }
-var_dump($num % 12 == 0 && $num % 24 == 0);
+
+$num = 72;
+$numberChecker = new NumberChecker($num);
+$result = $numberChecker->checkMultiples();
+echo $result;
+
+
+
+
+// $num = 72;
+
+// if ($num % 12 == 0 && $num % 24 == 0) {
+//   echo "numの値は12の倍数かつ、24の倍数です";
+// } elseif ($num % 12 == 0) {
+//   echo "numの値は12の倍数です";
+// } elseif ($num % 24 == 0) {
+//   echo "numの値は24の倍数です";
+// }
+
+// numの値は12の倍数かつ、24の倍数です
